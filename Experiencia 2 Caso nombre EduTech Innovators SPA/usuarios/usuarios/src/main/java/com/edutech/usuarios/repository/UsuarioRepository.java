c85d953755c4ede44a5abe5f62ffe177762237dc
ale reyes <72677443+moonnnluv@users.noreply.github.com>
2025-05-27 14:12:52 -0400

    Delete Experiencia 2 Caso nombre EduTech Innovators SPA/usuarios/usuarios directory

--- Experiencia 2 Caso nombre EduTech Innovators SPA/usuarios/usuarios/src/main/java/com/edutech/usuarios/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.usuarios.repository;
-
-import com.edutech.usuarios.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
