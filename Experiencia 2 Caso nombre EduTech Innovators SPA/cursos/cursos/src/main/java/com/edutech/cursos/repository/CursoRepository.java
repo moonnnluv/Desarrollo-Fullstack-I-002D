686cfa181ea8d5e5ff3b9e234b8a5913e12abe9f
ale reyes <72677443+moonnnluv@users.noreply.github.com>
2025-05-27 14:13:01 -0400

    Delete Experiencia 2 Caso nombre EduTech Innovators SPA/cursos/cursos directory

--- Experiencia 2 Caso nombre EduTech Innovators SPA/cursos/cursos/src/main/java/com/edutech/cursos/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.cursos.repository;
-
-import com.edutech.cursos.entity.Curso;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-}
