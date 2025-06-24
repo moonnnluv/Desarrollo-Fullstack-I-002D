6fbdf0a7499823cf78a16380c9ababfa91ad55bc
ale reyes <72677443+moonnnluv@users.noreply.github.com>
2025-06-24 13:22:30 -0400

    Error de path

--- Exp3_Reyes_Torrejon/contenido/src/main/java/com/edutech/contenido/repository/ContenidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edutech.contenido.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.contenido.entity.Contenido;
-
-public interface ContenidoRepository extends JpaRepository<Contenido, Long> {
-}
